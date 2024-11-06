d23c19d63e575cacaefc3ce3db071edf137b0a23
wilyJ80 <abjurandam@gmail.com>
2024-11-06 09:43:46 -0300

    update tag printing function

--- debug.c
+++ debug.c
@@ -37,6 +37,9 @@ int main(int argc, char *argv[]) {
       printf("<REALCON, %lf>", token.doubleValue);
     case SIGN:
       switch (token.signCode) {
+      case ASSIGN:
+        printf("<SN, ASSIGN>");
+        break;
       case PLUS:
         printf("<SN, PLUS>");
         break;
@@ -49,9 +52,6 @@ int main(int argc, char *argv[]) {
       case SLASH:
         printf("<SN, SLASH>");
         break;
-      case ASSIGN:
-        printf("<SN, ASSIGN>");
-        break;
       case OPEN_PAR:
         printf("<SN, OPEN_PAR>");
         break;
@@ -63,6 +63,39 @@ int main(int argc, char *argv[]) {
         break;
       case CLOSE_BRACK:
         printf("<SN, CLOSE_BRACK>");
+        break;
+      case COMMA:
+        printf("<SN, COMMA>");
+        break;
+      case COMPARISON:
+        printf("<SN, COMPARISON>");
+        break;
+      case REF:
+        printf("<SN, REF>");
+        break;
+      case AND:
+        printf("<SN, AND>");
+        break;
+      case OR:
+        printf("<SN, OR>");
+        break;
+      case SMALLER_THAN:
+        printf("<SN, SMALLER_THAN>");
+        break;
+      case LARGER_THAN:
+        printf("<SN, LARGER_THAN>");
+        break;
+      case NEGATION:
+        printf("<SN, NEGATION>");
+        break;
+      case DIFFERENT:
+        printf("<SN, DIFFERENT>");
+        break;
+      case SMALLER_EQUAL:
+        printf("<SN, SMALLER_EQUAL>");
+        break;
+      case LARGER_EQUAL:
+        printf("<SN, LARGER_EQUAL>");
         break;
       }
       break;
